4f2d1f8f7001bf03a39b2ab4da8b2f2199bde628
rahul.raviprasad <rahul.raviprasad@servicenow.com>
2017-03-12 22:34:23 +0530

    added more methods like adding labels_features, scaling, splitting data into training and test

--- script.py
+++ script.py
@@ -19,3 +19,16 @@ class DataProcessing:
     def get_data_from_csv(self):
         import pandas
         self.data_frame = pandas.read_csv(self.csv_file_path)
+
+    def get_features_and_labels(self):
+        import numpy
+        self.labels = numpy.array(self.data_frame['label'])
+        self.features = numpy.array(self.data_frame.drop(['label'], 1))
+
+    def do_scaling(self):
+        from sklearn.preprocessing import scale
+        scale(self.features)
+
+    def split_train_and_test_data(self):
+        from sklearn.model_selection import train_test_split
+        self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(self.features, self.label, test.size = 0.3)
